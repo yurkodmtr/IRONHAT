ac6b7128c41b3562525840d9a00a4b1bde3be8f3
Yurko Dmitriev <4au.4au.4au@gmail.com>
2017-10-01 16:51:02 +0300

    ghjk

--- html/js/scripts/myscript.js
+++ html/js/scripts/myscript.js
@@ -1,3 +1,39 @@
-$(window).load(function(){
-	console.log('asdf')
+"use strict";
+
+var siteScript = function(){
+
+	$('.mobile_nav__open').click(function(){
+		$('.mobile_nav').fadeIn();
+		$('body').css('overflow','hidden');
+	});
+
+	$('.mobile_nav .close').click(function(){
+		$('.mobile_nav').fadeOut();
+		$('body').css('overflow','visible');
+	});
+
+	var homeMenuItemHeight = function(){
+		var maxHeight = 0;
+		$('.home .menu .item').css('height','auto');
+		$('.home .menu .item').each(function(){
+			var h = $(this).outerHeight();
+			if ( h > maxHeight ) {
+				maxHeight = h;
+			}
+		});
+		$('.home .menu .item').css('height', maxHeight + 'px');
+	}
+
+	$(document).ready(function(){
+		homeMenuItemHeight();
+	});
+
+	$(window).resize(function(){
+		homeMenuItemHeight();
+	});
+
+}
+
+$(document).ready(function(){
+	siteScript();
 });
